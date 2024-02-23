dc6e64b3eb6c9e2e521e809c22eba4738de8bec0
ShahDhruvik <devep1210@gmail.com>
2024-02-23 12:10:58 +0530

    changes

---
+++ pm2.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'vite-react-app',
+      script: 'npm',
+      args: 'run dev',
+      interpreter: '/bin/bash',
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
